d0edd31b5c94f5b30b8a04643e4434d223bc3bd3
Faris Jiwad <fjiwad@gwu.edu>
2024-09-03 21:35:05 -0400

    Finally figured out the stupid hydration error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,31 +1,16 @@
-import Link from 'next/link'
+import React, { useEffect, useState } from "react";
 
-export default function Navbar(){
-    //Home, about, and skills I want to be on the same scrolling page (no need to be redundant)
-    //But posts, projects, and contact me I want on different pages 
-    return(
-        <div className=''>
-            <ul>
-                <li>
-                    <Link href="#home">Home</Link>
-                </li>
-                <li>
-                    <Link href="#about">About</Link>
-                </li>
-                <li>
-                    <Link href="/home">Posts</Link>
-                </li>
-                <li>
-                    <Link href="/home">Projects</Link>
-                </li>
-                <li>
-                    <Link href="#skills">Skills</Link>
-                </li>
-                <li>
-                    <Link href="/home">Contact Me</Link>
-                </li>
-
-            </ul>
-        </div>
+export default function Navbar() {
+    return (
+        <header className="navbar">
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Posts</li>
+                </ul>
+            </div>
+        </header>
     )
 }
