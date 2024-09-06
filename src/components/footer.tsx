aa665029374459bf6d081281ed061fa7f4cd6085
Faris Jiwad <fjiwad@gwu.edu>
2024-09-06 09:00:03 -0400

    Finally fixed the icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 
 export default function Footer(){
     return(
         <div className="bg-black pt-2">
-            <div className="text-center grid grid-cols-2">
-                <a href="https://github.com/Ferriswheel09/" target="_blank" className="flex justify-center items-center p-4 bg-slate-400">
-                    <FontAwesomeIcon className="max-h-10 mx-2 text-blue-500 hover:-translate-y-1  hover:cursor-pointer" icon={faGithub} />
+            <div className="flex flex-row align-center justify-center">
+                <a href="https://github.com/Ferriswheel09/" target="_blank" className="px-1">
+                    <img src="github-brands.png" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
                 </a>
-                <a href="https://www.linkedin.com/in/faris-jiwad/" className="flex justify-center items-center p-4">
-                    <FontAwesomeIcon className="max-h-10 mx-2 text-blue-500 hover:-translate-y-1  hover:cursor-pointer" icon={faLinkedin} />
+                <a href="https://www.linkedin.com/in/faris-jiwad/" className="px-1">
+                    <img src="linkedin-brands.png" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
                 </a>
             </div>
             <div className="text-center text-neutral-400 pb-2">
