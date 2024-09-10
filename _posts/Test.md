c2a54298525cc32e716135aad47a67cc31d02464
Faris Jiwad <fjiwad@gwu.edu>
2024-09-10 10:27:24 -0400

    Working on styling for blog page itself

--- _posts/Test.md
+++ _posts/Test.md
@@ -5,4 +5,5 @@ date: "09/05/2024"
 excerpt: "This is a test post that I'm trying to figure out how to deploy before actually creating the site so what happens if la dee da the text gets wayyyy longer than it should be"
 coverImage: "/_posts/test.jpg"
 ---
+# Adding a Title to see how it displays
 Lorem ipsum is this how this works
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import ReactMarkdown from 'react-markdown';
+
 
 const POSTS_DIRECTORY = path.join(process.cwd(), '_posts'); // Adjust to your posts directory
 
@@ -41,12 +43,37 @@ const BlogPost = async ({ params }: { params: { slug: string } }) => {
   const post = getPostBySlug(slug);
   const content = getContentAfterFrontMatter(POSTS_DIRECTORY + '\\' + post!.filename);
   const details = getDetails(post)
+  console.log(details)
 
   return (
-    <div className='mt-32'>
-      <h1>{details!.title}</h1>
-      <h2>{details!.date}</h2>
-      <p>{content}</p> 
+    <div className='mt-32 text-white'>
+      <div className='sm:ml-4 items-center bg-zinc-500 shadow-2xl shadow-zinc-500 sm:flex sm:flex-row'>
+        <div className='flex-1 pl-2'>
+          <h1 className='text-6xl font-bold mb-2'>{details!.title}</h1>
+          <h2 className='text-3xl'>{details!.date}</h2>
+        </div>
+        <div className="flex justify-center">
+          <img
+            src={details!.coverImage}
+            alt="Not loaded"
+            className="sm:max-w-xs object-center"
+          />
+        </div>
+      </div>
+
+      <ReactMarkdown 
+        className='text-center my-20'
+        components={{
+          h1: ({node, ...props}) => (
+            <h1 className="text-3xl font-bold text-blue-500" {...props} />
+          ),
+          p: ({ node, ...props }) => (
+            <p className="text-base text-gray-700" {...props} />
+          ),
+        }}
+      >
+        {content}
+      </ReactMarkdown> 
     </div>
   );
 };
