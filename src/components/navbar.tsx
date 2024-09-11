dc552b0ec68d956cd5b318cd2311469b669e6e1b
Faris Jiwad <fjiwad@gwu.edu>
2024-09-10 22:36:05 -0400

    Fixed the navbar to add some mobile features

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
@@ -6,11 +6,12 @@ import Home from "@/resources/home";
 import Info from "@/resources/about";
 import Monitor from "@/resources/project";
 import Bookmark from "@/resources/post";
-import Droplist from "./dropdown";
+import { useRouter } from 'next/router'; // Import useRouter
 
-export default function Navbar() {
-  const size = useWindowSize();
-  
+export default function Navbar() {  
+
+  const [dropdownState, setDropdownState] = useState(false);
+
   const navItems = [
     { key: 'home', href: '/#home', icon: <Home />, label: 'Home' },
     { key: 'about', href: '/#about', icon: <Info />, label: 'About' },
@@ -18,14 +19,36 @@ export default function Navbar() {
     { key: 'posts', href: '/posts', icon: <Bookmark />, label: 'Posts' },
   ];
 
-  if (size.width > 1500) {
+  
+
     return (
       <header className="navbar w-full z-50 text-black fixed top-0 left-0 right-0 mx-auto h-36">
         <div className="bg-gradient-to-r from-grad-purp to-grad-blue via-black via-55% flex items-center justify-between mx-auto p-4 border-2 shadow-xl shadow-neutral-600 whitespace-nowrap">
           <div className="border-2 rounded-3xl p-4 w-40 max-w-fit text-center bg-button-teal shadow-xl">
             <h1 className="font-medium">Faris Jiwad</h1>
           </div>
-          <ul className="font-medium flex flex-row justify-end pl-2 space-x-4">
+          {/* Dropdown menu for mobile screens */}
+          <div className="block md:hidden relative">
+            <button onClick={() => setDropdownState(!dropdownState)} className="transition bg-button-teal border-2 border-white rounded-3xl p-4 mx-2 hover:text-black hover:bg-slate-100 hover:shadow-xl">
+              Menu
+            </button>
+            <ul className={`${!dropdownState ? "hidden" : ""} absolute left-0 mt-2 w-48 bg-white shadow-lg border rounded-lg`}>
+              {navItems.map(item => (
+                <li key={item.key}>
+                  <Link
+                    href={item.href}
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-200"
+                  >
+                    {item.icon}
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Normal Menu for Regular Screens */}
+          <ul className="hidden font-medium md:flex flex-row justify-end pl-2 space-x-4">
             {navItems.map(item => (
               <li key={item.key} >
                 <Link
@@ -41,42 +64,4 @@ export default function Navbar() {
         </div>
       </header>
     );
-  } else {
-    return (
-      <header className="navbar min-w-full z-50 text-black fixed top-0 left-0 right-0 mx-auto h-36">
-        <div className="bg-gradient-to-r from-grad-purp to-grad-blue via-black via-55% flex items-center justify-between mx-auto p-4 border-2 shadow-xl shadow-neutral-600 whitespace-nowrap">
-          <div className="border-2 rounded-3xl p-4 w-40 max-w-fit text-center bg-button-teal shadow-xl">
-            <h1 className="font-medium">Faris Jiwad</h1>
-          </div>  
-          <Droplist navItems={navItems} />
-        </div>
-      </header>
-    );
-  }
-}
-
-function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
-  });
-
-  useEffect(() => {
-    function handleResize() {
-      if(window.innerWidth && window.innerHeight){
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-      
-    }
-
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return windowSize;
-}
+  } 
