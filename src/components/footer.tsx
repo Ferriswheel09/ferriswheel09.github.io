6fbbdcd9b99bc29ecb98a39d0376c8b6bed98f8d
Faris Jiwad <fjiwad@gwu.edu>
2025-03-17 19:22:34 -0400

    Trying something new...

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useTheme } from "next-themes";
+import Image from "next/image";
 
 
 
@@ -14,10 +15,10 @@ export default function Footer(){
         <div className={`${theme === 'dark' ? 'bg-neutral-900' : 'bg-neutral-100'} pt-2 border-t-2 z-50`}>
             <div className="flex flex-row align-center justify-center">
                 <a href="https://github.com/Ferriswheel09/" target="_blank" className="px-1">
-                    <img src="/github-brands.png" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
+                    <Image src="/github-brands.png" alt="Github" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
                 </a>
                 <a href="https://www.linkedin.com/in/faris-jiwad/" target="_blank" className="px-1">
-                    <img src="/linkedin-brands.png" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
+                    <Image src="/linkedin-brands.png" alt="LinkedIn" className="h-10 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"/>
                 </a>
             </div>
             <div className="text-center text-neutral-400 pb-2">
